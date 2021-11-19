19a23516699f0d30b8048b4f18fdb3603993b8ee
davinci360 <jasonbrowntime@gmail.com>
2021-11-19 10:37:59 -0500

    1 more

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Header() {
     return (
-        <>
+        <div className="header">
         <header>PokeDatingzZ</header>
-        </>
+        </div>
     )
 } 
 
